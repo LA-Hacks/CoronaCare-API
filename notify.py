6d0abc001b3687702d0bb5d80451dcdca14b2f2b
Stanley K <skogin5@gmail.com>
2020-03-29 05:35:48 -0700

    Unified messages to fit with both sender and reciever

--- notify.py
+++ notify.py
@@ -12,29 +12,18 @@ class Notifier:
         self.twilio_number = args.get('twilio_number')
         self.to_number = args.get('to_number')
 
-# hospital is boolean specifying if message is for hospital (true) or provider (false),  status is package "prep", "transit", or "arrived" update, source is who is sending, source is person who is sending message, quantity and type specify nature of package
-    def send_notif(self, hospital, status, source, quantity, supply):
+# status is package "prep", "transit", or "arrived" update, source is who is sending, source is person who is sending message, quantity and type specify nature of package
+    def send_notif(self, status, source, quantity, supply):
         return self.sms.messages.create(
             from_=self.twilio_number,
             to=self.to_number,
-            if hospital:
-            if status == prep:
-            body="Your order from " + source + " of " + quantity +
+            if status == "prep":
+                body="Your order with " + source + " of " + quantity +
             " " + supply + "s" + " has been recieved."
-            elif status == transit
-            body="Your package from " + source + " of " + quantity +
+            elif status == "transit":
+                body="Your package with " + source + " of " + quantity +
             " " + supply + "s" + " is on route! Hold tight!"
             else:
-            body="Hooray! Your package of " + quantity + " " +
-            supply + " from " + source + " has arrived!"
-            else:
-            if status == prep:
-            body="Your order to " + source + " of " + quantity +
-            " " + supply + "s" + " is being processed."
-            elif status == transit
-            body="Your package to " + source + " of " +
-            quantity + " " + supply + "s" + " has been shipped"
-            else:
-            body="Hooray! Your package to " + quantity + " " +
-            supply + " from " + source + " was delivered!"
-                else: )
+                body="Hooray! Your package of " + quantity + " " +
+            supply + " with " + source + " was delivered!"
+        )
